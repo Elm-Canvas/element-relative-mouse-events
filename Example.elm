496c6ff3d3b632a92e059a841c9d8398f17debd8
Chadtech <chadstearns1@gmail.com>
2017-06-28 22:02:16 -0400

    Made an example file, and put the actual package in src

---
+++ Example.elm
@@ -0,0 +1,60 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, p, text)
+import Html.Attributes exposing (style)
+import ElementRelativeMouseEvents as Events exposing (Point)
+
+
+
+main =
+    Html.beginnerProgram
+        { model = Nothing 
+        , update = update
+        , view = view
+        }
+
+type Msg
+    = Click Point
+
+
+update : Msg -> Maybe Point -> Maybe Point
+update msg maybePoint =
+    case msg of
+        Click point ->
+            Just point
+
+
+view : Maybe Point -> Html Msg
+view maybePoint =
+    div
+        []
+        [ p [] [ text "Click on this blue div"]
+        , div
+            [ style
+                [ ("margin-left", "40px")
+                , ("margin-top", "40px")
+                , ("margin-bottom", "40px")
+                , ("width", "200px")
+                , ("height", "200px")
+                , ("background", "#0000ff")
+                ]
+            , Events.onClick Click
+            ]
+            []
+        , pointView maybePoint
+        ]
+
+
+pointView : Maybe Point -> Html Msg
+pointView maybePoint =
+    case maybePoint of
+        Nothing ->
+            p [] [ text "No click yet" ]
+
+        Just { x, y } ->
+            p [] [ text (pointText x y )]
+
+
+pointText : Float -> Float -> String
+pointText x y =
+    "Last click was at (" ++ (toString x) ++ ", " ++ (toString y) ++ ")"
